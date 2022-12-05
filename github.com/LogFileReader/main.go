3806183e664442636dc0b12610c4bac239305f6e
dpandya <dpandya@riotgames.com>
2022-12-05 14:35:28 -0800

    Logfile processor

---
+++ github.com/LogFileReader/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	fmt.Println("SP// Backend Developer Test - Input Processing")
+	fmt.Println()
+
+	// Read STDIN into a new buffered reader
+	reader := bufio.NewReader(os.Stdin)
+	//calling log processor....
+	Process(reader)
+
+}
+
+// Process() funcation will read the input by spilting lines with \n.
+// And pass chunks from input to ProcessChunk() in memory.
+func Process(f *bufio.Reader) error {
+
+	linesPool := sync.Pool{New: func() interface{} {
+		lines := make([]byte, 250*1024)
+		return lines
+	}}
+
+	stringPool := sync.Pool{New: func() interface{} {
+		lines := ""
+		return lines
+	}}
+
+	r := bufio.NewReader(f)
+
+	var wg sync.WaitGroup
+
+	for {
+		buf := linesPool.Get().([]byte)
+
+		n, err := r.Read(buf)
+		buf = buf[:n]
+
+		if n == 0 {
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		nextUntillNewline, err := r.ReadBytes('\n')
+
+		if err != io.EOF {
+			buf = append(buf, nextUntillNewline...)
+		}
+
+		wg.Add(1)
+		go func() {
+			ProcessChunk(buf, &linesPool, &stringPool)
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+	return nil
+}
+
+// Funcation will proces the chunk and pullout error line and print.
+// Simple search based on string error you can search using timestamp as well.
+func ProcessChunk(chunk []byte, linesPool *sync.Pool, stringPool *sync.Pool) {
+
+	var wg2 sync.WaitGroup
+
+	logs := stringPool.Get().(string)
+	logs = string(chunk)
+
+	linesPool.Put(chunk)
+
+	logsSlice := strings.Split(logs, "\n")
+
+	stringPool.Put(logs)
+
+	chunkSize := 300
+	n := len(logsSlice)
+	noOfThread := n / chunkSize
+
+	if n%chunkSize != 0 {
+		noOfThread++
+	}
+
+	for i := 0; i < (noOfThread); i++ {
+
+		wg2.Add(1)
+		go func(s int, e int) {
+			defer wg2.Done() //to avaoid deadlocks
+			for i := s; i < e; i++ {
+				text := logsSlice[i]
+				if len(text) == 0 {
+					continue
+				}
+				status := strings.Contains(text, "error")
+
+				if status {
+					fmt.Println(text)
+				}
+			}
+
+		}(i*chunkSize, int(math.Min(float64((i+1)*chunkSize), float64(len(logsSlice)))))
+	}
+
+	wg2.Wait()
+	logsSlice = nil
+}
